8cbfc39bdcc02295f723a4923870b7c2647603d2
Pritam Paul <pritam.paul@verizonmedia.com>
2023-08-08 14:04:38 -0400

    fix external script (#17)
    Co-authored-by: ppaul <pritam.paul@yahooinc.com>

--- modules/kms/get-kms-key.sh
+++ modules/kms/get-kms-key.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+# This script is run as terraform external data source to fetch kms key id
 set -e
 
-alias_name=$1
-region=$2
+eval "$(jq -r '@sh "alias_name=\(.alias_name) region=\(.region)"')"
 
-key_id=$(aws kms describe-key --key-id $alias_name --region $region --query 'KeyMetadata.KeyId' --output text 2>/dev/null || echo "")
+RESULT=`aws kms describe-key --key-id $alias_name --region $region --query 'KeyMetadata.KeyId' --output text 2>/dev/null || echo ""`
 
-echo "{\"key_id\":\"$key_id\"}"
+jq -n --arg key_id "$RESULT" '{"key_id":$key_id}'
