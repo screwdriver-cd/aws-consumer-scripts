6b9ac2de0fc7537aba1ea79824955e0a17bb7119
ppaul <pritam.paul@yahooinc.com>
2023-08-08 11:59:37 -0400

    fix resources

--- modules/kms/main.tf
+++ modules/kms/main.tf
@@ -32,7 +32,7 @@ locals {
 }
 
 # Create new KMS key if it doesn't exist
-resource "aws_kms_key" "sd_build_kms_key" {
+resource "aws_kms_key" "new_sd_build_kms_key" {
   count               = local.existing_sd_build_kms_key_id == "" ? 1 : 0
   description         = "KMS Key for Screwdriver Builds"
   enable_key_rotation = true
@@ -77,7 +77,7 @@ EOF
 
 # Determine which KMS key to use
 locals {
-  sd_build_kms_key_id = local.existing_sd_build_kms_key_id != "" ? local.existing_sd_build_kms_key_id : aws_kms_key.sd_build_kms_key.0.key_id
+  sd_build_kms_key_id = local.existing_sd_build_kms_key_id != "" ? local.existing_sd_build_kms_key_id : aws_kms_key.new_sd_build_kms_key.0.key_id
 }
 
 
@@ -86,4 +86,15 @@ resource "aws_kms_alias" "sd_build_kms_key_alias" {
   count         = local.existing_sd_build_kms_key_id == "" ? 1 : 0
   name          = "alias/${var.kms_key_alias_name}"
   target_key_id = local.sd_build_kms_key_id
+}
+
+
+moved {
+  from = module.kms.aws_kms_key.sd_build_kms_key_alias
+  to = module.kms.aws_kms_alias.sd_build_kms_key_alias[0]
+}
+
+moved {
+  from = module.kms.aws_kms_key.sd_build_kms_key
+  to =  module.kms.aws_kms_key.new_sd_build_kms_key[0]
 }
